51922fbd8d4d2f6e463170824fed8611c80ee49f
korimis <korimis@shure.com>
2025-10-12 19:28:43 +0530

    Adds error handling logic

--- OnlineCompiler/submit/views.py
+++ OnlineCompiler/submit/views.py
@@ -17,8 +17,7 @@ def submit(request):
             )
             submission.output_data = output
             # submission.save() Do we need this?
-            return render(request, 'result.html', { 'submission': submission })
-
+            return render(request, 'form.html', { 'submission': submission, 'form': form })
     else:
         form = CodeSubmissionForm()
         context = {
@@ -27,66 +26,72 @@ def submit(request):
     return render(request, 'form.html', context)
     
 def run_code(language, code, input_data):
-    project_path = Path(settings.BASE_DIR)
-    directories = ["codes", "inputs", "outputs"]
-    # create these folders in the project to store inputs, outputs and code
-    for directory in directories:
-        dir_path = project_path / directory
-        if not dir_path.exists():
-            dir_path.mkdir(parents=True, exist_ok=True)
 
-    codes_dir = project_path / directories[0]
-    inputs_dir = project_path / directories[1]
-    outputs_dir = project_path / directories[2]
+    
+    project_path = Path(settings.BASE_DIR)
+    # create these folders in the project to store inputs, outputs and code
+    dir_path = project_path / "codes"
+    if not dir_path.exists():
+        dir_path.mkdir(parents = True, exist_ok = True)
+
+    codes_dir = project_path / "codes"
 
     unique = str(uuid.uuid4())
 
     code_file_name = f"{unique}.{language}"
-    input_file_name = f"{unique}.txt"
-    output_file_name = f"{unique}.txt"
 
     code_file_path = codes_dir / code_file_name
-    input_file_path = inputs_dir / input_file_name
-    output_file_path = outputs_dir / output_file_name
 
-    with open(code_file_path, "w") as code_file:
-        code_file.write(code)
-    
-    with open(input_file_path, "w") as input_file:
-        input_file.write(input_data)
-    
-    with open(output_file_path, "w") as output_file:
-        pass
+    def compileAndRun(forLang):
+        with open(code_file_path, "w") as code_file:
+            code_file.write(code)
 
-    if language == 'cpp':
         executable_path = codes_dir / unique
         compile_result = subprocess.run(
-            ["clang++", str(code_file_path), "-o", str(executable_path)]
+            [forLang, str(code_file_path), "-o", str(executable_path)],
+            capture_output = True,
+            text = True
         )
 
         # Need to handle the errors and show to the users on the same page with user typed text in form
         if compile_result.returncode == 0:
-            with open(input_file_path, "r") as input_file:
-                with open(output_file_path, "w") as output_file:
-                    subprocess.run(
-                        [str(executable_path)],
-                        stdin = input_file,
-                        stdout = output_file,
-                    )
+            process = subprocess.run(
+                [str(executable_path)],
+                input = input_data,
+                capture_output = True,
+                text = True
+            )
+            if process.returncode == 0:
+                return process.stdout
+            return getError(str(process.stderr).strip(), str(code_file_path))
+        return getError(str(compile_result.stderr).strip(), str(code_file_path))
+
+    if language == 'c':
+        return compileAndRun('gcc')
+    
+    if language == 'cpp':
+        return compileAndRun('clang++')
         
     if language == 'py':
-        with open(input_file_path, "r") as input_file:
-            with open(output_file_path, "w") as output_file:
-                subprocess.run(
-                    ["python3", str(code_file_path)],
-                    stdin = input_file,
-                    stdout = output_file,
-                )
-    
-    with open(output_file_path, "r") as output_file:
-        output_data = output_file.read()
-    
-    return output_data
+        with open(code_file_path, "w") as code_file:
+            code_file.write(code)
+        process = subprocess.run(
+            ["python3", str(code_file_path)],
+            input = input_data,
+            capture_output = True,
+            text = True
+        )
+        
+        if process.returncode == 1:
+            return getError(process.stderr, str(code_file_path))
+        return process.stdout
+        
+
+def getError(error_string, code_file_path):
+    newString = error_string.replace(str(code_file_path), "Sumbission file ")
+    return newString
+
+
 
 
 
